a7b773e8a0fe8c1fc32e56cb699effcbbbc34b54
Wes Morgan <git@wesmorgan.me>
2023-12-14 12:17:43 -0700

    Delete test boilerplate for now

--- test/fluree/kinesis_component_test.clj
+++
@@ -1,7 +0,0 @@
-(ns fluree.kinesis-component-test
-  (:require [clojure.test :refer :all]
-            [com.fluree.kinesis-component :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
