691b97acd22da9b6df18ecdaf07aae1c23d84998
Wes Morgan <git@wesmorgan.me>
2024-07-15 12:18:45 -0600

    Convert from donut to integrant

--- src/fluree/kinesis.clj
+++
@@ -1,7 +0,0 @@
-(ns fluree.kinesis
-  (:require [fluree.kinesis.client]
-            [fluree.kinesis.stream]))
-
-(def client fluree.kinesis.client/component)
-(def stream fluree.kinesis.stream/component)
-(def publish-record fluree.kinesis.stream/publish-record)
